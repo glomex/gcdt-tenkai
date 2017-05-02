8eb4b3029963f2659448b150ea558d66d87517c5
mark <mark@mark-fink.de>
2017-05-02 17:00:22 +0200

    #283 prepare for python3

--- tests/test_tenkai.py
+++ tests/test_tenkai.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function
 
-from nose.tools import assert_equal, assert_items_equal
-
 from gcdt.tenkai_core import _build_bundle_key
 
 from gcdt_testtools.helpers import temp_folder  # fixtures!
@@ -11,4 +9,4 @@ from gcdt_testtools.helpers import temp_folder  # fixtures!
 def test_build_bundle_key():
     application_name = 'sample_name'
     expected = '%s/bundle.tar.gz' % application_name
-    assert_equal(_build_bundle_key(application_name), expected)
+    assert _build_bundle_key(application_name) == expected
