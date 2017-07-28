862882ea8425b868aa3876af48592755a4c9c0a5
mark <mark@mark-fink.de>
2017-07-28 20:23:41 +0200

    #264 more flexible event wiring

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function
 import os
+import logging
 
 
 def here(p):
     return os.path.abspath(os.path.join(os.path.dirname(__file__), p))
+
+logging.basicConfig()
