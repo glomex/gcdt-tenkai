09a12627197dbdf27f74e85577b9fea7d03424bb
mark <mark@mark-fink.de>
2017-07-31 18:00:33 +0200

    #264 added tests and made them work

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,8 +3,9 @@ from __future__ import unicode_literals, print_function
 import os
 import logging
 
+logging.basicConfig(level=logging.INFO)
+
 
 def here(p):
     return os.path.abspath(os.path.join(os.path.dirname(__file__), p))
 
-logging.basicConfig()
